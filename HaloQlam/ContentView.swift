26d2d354365f3451b575b6cc280a9c06ef038bb9
naimsassine <naim.sassine@hotmail.com>
2024-12-02 21:16:38 +0100

    Initial Commit

---
+++ HaloQlam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HaloQlam
+//
+//  Created by naim sassine on 02/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HaloQlam/HaloQlamApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaloQlamApp.swift
+//  HaloQlam
+//
+//  Created by naim sassine on 02/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct HaloQlamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
